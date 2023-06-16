c1f56523eef89496065217e438bd5200b58e15c2
HAFUSAA <hssa48845@gmail.com>
2023-06-16 10:14:49 -0700

    a

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
+ * Description: A C program that prints with printf function.
+ *
  * Return: Always 0 (Success)
- */
+*/
+
 int main(void)
-{       
-        puts("\"with proper grammar, but the outcome is a piece of art,\n"):
-        return (0):
-}                       
+{
+	puts("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
 
