c0b53c230af63fc95bb6fe0c364c90ebc6a11f3e
HAFUSAA <hssa48845@gmail.com>
2023-06-21 07:31:37 -0700

    a

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - entry point
+ * Description: add two value
+ * @n1: the first numb to computed
+ * @n2: the second numb to computed
+ * Return: integer value
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
