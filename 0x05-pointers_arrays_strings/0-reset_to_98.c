78e43f7d4b783330127a43474da768430f43763c
HAFUSAA <hssa48845@gmail.com>
2023-06-26 15:14:21 -0700

    a

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - entry point 
+ * Desccription: takes a pointer to an int 
+ * @n: int parameter
+ * Return: nothing
+ */
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
