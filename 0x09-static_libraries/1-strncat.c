819bd35bf1722b22961762872baf1762585f982c
HAFUSAA <hssa48845@gmail.com>
2023-07-08 10:30:16 -0700

    z

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -7,11 +7,11 @@
  * @n: input value
  * Return: dest
 */
-char *_strncat (char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	int j;
-	
+
 	i = 0;
 	while (dest[i] != '\0')
 	{
